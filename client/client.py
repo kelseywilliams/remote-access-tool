b72f95830c027143703d97e90cec4b2b8044a305
Kelsey Williams <kelseywilliams1200@gmail.com>
2023-09-10 17:08:05 -0700

    object oriented approach utilizing websockets lib

--- client/client.py
+++ client/client.py
@@ -1,23 +1,129 @@
 import asyncio
+import websockets
+from websockets import WebSocketClientProtocol
 import mss
 from screeninfo import get_monitors
-import time
+import numpy as np
+import sys
+import logging
+from time import time
+np.set_printoptions(threshold=sys.maxsize)
 
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.uri = f"ws://{self.host}:{self.port}"
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.logger = _create_logger()
+        self._server_timeout = 5
+        self._wake_up_task = None
+    def start(self):
+        #self.logger.info(f"Connected to {self.host}:{self.port}")
+        self.loop.run_until_complete(self.connect())
+        self._wake_up_task = asyncio.ensure_future(_wake_up(), loop=self.loop)
+
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            self.logger.debug("caught keyboard interrupt")
+            self.exit()
+
+    async def connect(self):
+        ws_client = await websockets.connect(self.uri)
+        self.logger.info(f"Connected to {self.host}:{self.port}")
+        keep_alive_task = asyncio.ensure_future(self.keep_alive(ws_client))
+        try:
+            await self.handler(ws_client)
+        except websockets.ConnectionClosed:
+            keep_alive_task.cancel()
+            await self.disconnect(ws_client)
+    
+    async def handler(self, server: WebSocketClientProtocol):
+        while True:
+            message = str(time())
+            await server.send(message)
+            self.logger.info(f"Sent {message} to {server.remote_address}")
+    
+    async def disconnect(self, server):
+        await server.close()
+        self.logger.info(f"Disconnected from server at {self.host}:{self.port}")
+        
+    async def keep_alive(self, server: WebSocketClientProtocol):
+        while True:
+            await asyncio.sleep(self._server_timeout)
+            try:
+                self.logger.debug(f"pinging {self.host}:{self.port}")
+                await asyncio.wait_for(server.ping(), self._server_timeout)
+            except asyncio.TimeoutError:
+                self.logger.info(f"Server {self.host}:{self.port} timed out")
+    
+    def exit(self):
+        self.logger.info("exiting")
+        self._wake_up_task.cancel()
+        try:
+            self.loop.run_until_complete(self._wake_up_task)
+        except asyncio.CancelledError:
+            self.loop.close()
+
+def _create_logger():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("remote-access-tool.server")
+    logger.setLevel(logging.INFO)
+    # ws_logger is never used
+    ws_logger = logging.getLogger('websockets.server')
+    ws_logger.setLevel(logging.ERROR)
+    ws_logger.addHandler(logging.StreamHandler())
+    return logger
+
+async def _wake_up():
+    while True:
+        await asyncio.sleep(1)
+
+def main():
+    client = Client("localhost", 80)
+    client.start()
+
+if __name__ == "__main__":
+    main()
+
+'''
 async def main():
     reader, writer = await asyncio.open_connection("127.0.0.1", 8080)
     monitors = get_monitors()
     monitor = monitors[0]
     bounding_box = {"top":0, "left":0, "width":monitor.width, "height":monitor.height}
     sct = mss.mss()
+
     while True:
+        # Grab screen image
         sct_img = sct.grab(bounding_box)
         sct_img = mss.tools.to_png(sct_img.rgb, sct_img.size)
-        img_size = sct_img.__sizeof__()
-        print(img_size)
-        writer.write(sct_img)
-        writer.write("\n".encode())
+        #sct_img = zlib.compress(sct_img)
+        # Print size of image
+        img_size = sys.getsizeof(sct_img)
+        img_size_str = str(img_size)
+
+        # Send the image size to the server
+        writer.write(img_size_str.encode())
+        writer.write(b"\n")
         await writer.drain()
-        time.sleep(15)
+        
+        # Break up image into 64kb chunks
+        # Start by turning the image array into a numpy array
+        img_array = np.frombuffer(sct_img, dtype=np.uint8)
+        # The image will be broken up into 64kb chunks.
+        chunks = np.array_split(img_array, np.arange(CHUNK_SIZE, len(img_array), CHUNK_SIZE))
+        # ***** TODO Remove this test code *****
+        total_mem = 0
+        for chunk in chunks:
+            print(chunk.nbytes)
+            total_mem += chunk.nbytes
+        print(f"Len of chunks: {len(chunks)}, Size of chunks: {total_mem}")
 
-asyncio.run(main())
-
+        chunks_length = len(chunks)
+        for i in range(chunks_length):
+            writer.writelines(chunks[i])
+            await writer.drain()
+            '''
--- server/server.py
+++ server/server.py
@@ -1,44 +1,98 @@
-import socket
+# Base code written by stefanotorresi
+# https://gist.github.com/stefanotorresi/bfb9716f73f172ac4439436456a96d6f
+
+import websockets
+from websockets import WebSocketServerProtocol
 import asyncio
-import cv2
-import numpy as np
-from typing import Deque, DefaultDict, Dict
-import mss
-from screeninfo import get_monitors
-host = "127.0.0.1"
-port = 8080
+import logging
 
-def show_image(data):
-    # read image as an numpy array
-    image = np.asarray(bytearray(data), dtype="uint8")
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.clients = set()
+        # Loop handling set to satisfy new py 3.11 asyncio reqs https://stackoverflow.com/questions/73361664
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.logger = _create_logger()
+        self._client_timeout = 5
+        self._wake_up_task = None
+
+    def listen(self): 
+        self.logger.info(f"listening on {self.host}:{self.port}")
+        ws_server = websockets.serve(self.connect_client, self.host, self.port)
         
-    # use imdecode function
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        self.loop.run_until_complete(ws_server)
+        self._wake_up_task = asyncio.ensure_future(_wake_up(), loop=self.loop)
 
-    cv2.imshow("screen", image)
-    if(cv2.waitKey(1) & 0xFF) == ord("q"):
-        cv2.destroyAllWindows()
+        # TODO Implement shell mode and silent mode
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            self.logger.debug('caught keyboard interrupt')
+            self.exit()
 
-async def client_connected(reader, writer):
-    addr = writer.get_extra_info("peername")
-    print(f"{addr} has connected.")
+    async def connect_client(self, client: WebSocketServerProtocol, path):
+        self.clients.add(client)
+        self.logger.info('new client connected from {}:{}'.format(*client.remote_address))
+        keep_alive_task = asyncio.ensure_future(self.keep_alive(client))
+        try:
+            await self.handle_messages(client)
+        except websockets.ConnectionClosed:
+            keep_alive_task.cancel()
+            await self.disconnect_client(client)
 
+    async def handle_messages(self, client):
+        while True:
+            message = await client.recv()
+            self.logger.info(f"recieved message from {client.remote_address}: {message}")
+            #await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def disconnect_client(self, client):
+        await client.close()
+        self.clients.remove(client)
+        self.logger.info('client {}:{} disconnected'.format(*client.remote_address))
+
+    # TODO Determine: is this a necessary function? Does it simply poll client?
+    async def keep_alive(self, client: WebSocketServerProtocol):
+        while True:
+            await asyncio.sleep(self._client_timeout)
+            try:
+                self.logger.debug('pinging {}:{}'.format(*client.remote_address))
+                await asyncio.wait_for(client.ping(), self._client_timeout)
+            except asyncio.TimeoutError:
+                self.logger.info('client {}:{} timed out'.format(*client.remote_address))
+                await self.disconnect_client(client)
+
+    def exit(self):
+        self.logger.info("exiting")
+        self._wake_up_task.cancel()
+        try:
+            self.loop.run_until_complete(self._wake_up_task)
+        except asyncio.CancelledError:
+            self.loop.close()
+
+
+def _create_logger():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("remote-access-tool.server")
+    logger.setLevel(logging.INFO)
+    # ws_logger is never used
+    ws_logger = logging.getLogger('websockets.server')
+    ws_logger.setLevel(logging.ERROR)
+    ws_logger.addHandler(logging.StreamHandler())
+    return logger
+
+
+async def _wake_up():
     while True:
-        recvd = b""
-        while data := await reader.readline():
-            recvd = recvd + data 
-            print(data)
-        recvd = recvd[:-1]
-        print(f"Total:{recvd.__sizeof__()}")
+        await asyncio.sleep(1)
 
-        show_image(data) 
-        
-async def main():
-    server = await asyncio.start_server(client_connected, host, port)
-    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
 
-    async with server:
-        await server.serve_forever()
+def main():
+    server = Server("localhost", 80)
+    server.listen()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    main()
